7b0b3fc102eb023463d32eaa821591d6f93b9837
Jogendra Singh <imjog24@gmail.com>
2019-06-05 13:10:33 +0530

    Intial commit

--- SwiftUI-TutorialApp/ContentView.swift
+++ SwiftUI-TutorialApp/ContentView.swift
@@ -10,9 +10,18 @@ import SwiftUI
 
 struct ContentView : View {
     var body: some View {
-        Text("Hello Jogendra")
-        .font(.title)
-        .color(.gray)
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
     }
 }
 
